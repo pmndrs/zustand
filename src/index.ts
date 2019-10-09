f09c186eb49e9a0345c618edadfafa43c15e7f8a
JeremyHolcomb <jholcomb@kyriba.com>
2019-10-09 00:32:48 -0700

    Update subscriber order logic

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
 import { useEffect, useLayoutEffect, useReducer, useRef } from 'react'
 
 export type State = Record<string | number | symbol, any>
-export type StateListener<T> = (state: T) => void
+export interface StateListener<T> {
+  (state: T, error: null): void
+  (state: null, error: Error): void
+}
 export type StateSelector<T extends State, U> = (state: T) => U
 export type PartialState<T extends State> =
   | Partial<T>
   | ((state: T) => Partial<T>)
 export type EqualityChecker<T> = (state: T, newState: any) => boolean
-export interface SubscribeOptions<T extends State, U> {
-  selector?: StateSelector<T, U>
-  equalityFn?: EqualityChecker<U>
-  currentSlice?: U
+export interface UseStoreSubscribeOptions<T extends State, U> {
+  selector: StateSelector<T, U>
+  equalityFn: EqualityChecker<U>
+  currentSlice: U
+  listenerIndex: number
   subscribeError?: Error
-  listenerIndex?: string
 }
+export type SubscribeOptions<T extends State, U> = Partial<
+  UseStoreSubscribeOptions<T, U>
+>
 export type StateCreator<T extends State> = (
   set: SetState<T>,
   get: GetState<T>,
@@ -22,7 +28,7 @@ export type StateCreator<T extends State> = (
 export type SetState<T extends State> = (partial: PartialState<T>) => void
 export type GetState<T extends State> = () => T
 export type Subscribe<T extends State> = <U>(
-  listener: StateListener<U | void>,
+  listener: StateListener<U>,
   options?: SubscribeOptions<T, U>
 ) => () => void
 export type Destroy = () => void
@@ -37,10 +43,6 @@ export interface StoreApi<T extends State> {
   destroy: Destroy
 }
 
-// For using symbols as indexers: https://github.com/Microsoft/TypeScript/issues/24587
-const getOwnPropertySymbols = (Object.getOwnPropertySymbols as unknown) as (
-  o: any
-) => string[]
 const forceUpdateReducer = (state: number) => state + 1
 // For server-side rendering: https://github.com/react-spring/zustand/pull/34
 const useIsoLayoutEffect =
@@ -49,38 +51,44 @@ const useIsoLayoutEffect =
 export default function create<TState extends State>(
   createState: StateCreator<TState>
 ): [UseStore<TState>, StoreApi<TState>] {
+  // All listeners are wrapped in a function with the signature: () => void
+  const listeners: (() => void)[] = []
   let state: TState
-  let listeners: Record<string, StateListener<void> | null> = {}
+  let renderCount = 0
+
+  // Returns an int for a component based on render order.
+  function useRenderId() {
+    const renderIdRef = useRef<number>()
+    if (!renderIdRef.current) {
+      renderIdRef.current = renderCount++
+    }
+    return renderIdRef.current
+  }
 
   const setState: SetState<TState> = partial => {
     const partialState =
       typeof partial === 'function' ? partial(state) : partial
     if (partialState !== state) {
       state = Object.assign({}, state, partialState)
-      // listeners is an object to ensure enumeration order.
-      // Parent component's listeners must be called before their children's.
-      // https://github.com/react-spring/zustand/issues/64
-      getOwnPropertySymbols(listeners).forEach(listenerIndex => {
-        const listener = listeners[listenerIndex]
-        if (listener) listener()
-      })
+      listeners.forEach(listener => listener())
     }
   }
 
   const getState: GetState<TState> = () => state
 
   const subscribe: Subscribe<TState> = <StateSlice>(
-    listener: StateListener<StateSlice | void>,
+    listener: StateListener<StateSlice>,
     options: SubscribeOptions<TState, StateSlice> = {}
   ) => {
     if (!('currentSlice' in options)) {
       options.currentSlice = (options.selector || getState)(state)
     }
-    // options.listenerIndex is not mutable to ensure the correct listener is removed on cleanup.
-    const { listenerIndex = (Symbol() as unknown) as string } = options
-    const listenerFn = () => {
-      // Destructure in the listener to get current values. We rely on this
-      // because options is mutated in useStore.
+    // subscribe can be called externally without passing in a listenerIndex so
+    // we need to assign it a default index.
+    const { listenerIndex = renderCount++ } = options
+    const listenerWrapper = () => {
+      // Access the current values of the options object in listenerWrapper.
+      // We rely on this because options is mutated in useStore.
       const { selector = getState, equalityFn = Object.is } = options
       // Selector or equality function could throw but we don't want to stop
       // the listener from being called.
@@ -88,57 +96,44 @@ export default function create<TState extends State>(
       try {
         const newStateSlice = selector(state)
         if (!equalityFn(options.currentSlice as StateSlice, newStateSlice)) {
-          listener((options.currentSlice = newStateSlice))
+          listener((options.currentSlice = newStateSlice), null)
         }
       } catch (error) {
         options.subscribeError = error
-        listener()
+        listener(null, error)
       }
     }
-    listeners[listenerIndex] = listenerFn
-    return () => void delete listeners[listenerIndex]
+    listeners[listenerIndex] = listenerWrapper
+    // Intentially using delete because shortening the length of the listeners
+    // array would result in listenerIndex not accessing the correct listener.
+    // This means listeners should be considered a sparce array.
+    return () => delete listeners[listenerIndex]
   }
 
-  const destroy: Destroy = () => (listeners = {})
+  const destroy: Destroy = () => (listeners.length = 0)
 
   const useStore = <StateSlice>(
     selector: StateSelector<TState, StateSlice> = getState,
     equalityFn: EqualityChecker<StateSlice> = Object.is
   ) => {
-    if (Array.isArray(equalityFn)) {
-      equalityFn = Object.is
-      console.warn(
-        'Zustand: the 2nd arg for dependencies was deprecated in 1.0. Please remove it! See: https://github.com/react-spring/zustand#selecting-multiple-state-slices'
-      )
-    }
-
-    const isInitial = useRef(true)
-    const options = useRef(
-      // isInitial prevents the selector from being called every render.
-      isInitial.current && {
+    const listenerIndex = useRenderId()
+    const optionsRef = useRef<UseStoreSubscribeOptions<TState, StateSlice>>()
+    if (!optionsRef.current) {
+      optionsRef.current = {
         selector,
         equalityFn,
-        currentSlice: ((isInitial.current = false), selector(state)),
-        listenerIndex: (Symbol() as unknown) as string,
+        currentSlice: selector(state),
+        listenerIndex,
       }
-    ).current as SubscribeOptions<TState, StateSlice>
+    }
+    const options = optionsRef.current
 
     // Update state slice if selector has changed or subscriber errored.
     if (selector !== options.selector || options.subscribeError) {
       const newStateSlice = selector(state)
-      if (!equalityFn(options.currentSlice as StateSlice, newStateSlice)) {
+      if (!equalityFn(options.currentSlice, newStateSlice)) {
         options.currentSlice = newStateSlice
       }
-    }
-
-    // An object's keys are enumerated by insertion order.
-    // Insert a key for the listener during the component's render phase.
-    // This ensures the listeners are ordered based on the render order (parent to child).
-    // If we did not do this, the listeners would be inserted in useEffect.
-    // useEffect call order seems to be reversed (child effects are called before the parent's).
-    const listenerIndex = options.listenerIndex as string
-    if (!(listenerIndex in listeners)) {
-      listeners[listenerIndex] = null
     }
 
     useIsoLayoutEffect(() => {
@@ -147,7 +142,7 @@ export default function create<TState extends State>(
       options.subscribeError = undefined
     })
 
-    const forceUpdate = useReducer(forceUpdateReducer, 1)[1]
+    const forceUpdate = useReducer(forceUpdateReducer, 0)[1]
     useIsoLayoutEffect(() => subscribe(forceUpdate, options), [])
 
     return options.currentSlice
