302b9d0204ef5baf100af489143ff4c37730d159
Paul Henschel <drcmda@gmail.com>
2020-08-14 15:03:45 +0200

    example code

--- example/src/App.js
+++ example/src/App.js
@@ -2,25 +2,65 @@ import * as THREE from 'three'
 import React, { Suspense, useRef, useState } from 'react'
 import { Canvas, useFrame } from 'react-three-fiber'
 import { Plane, useAspect, useTextureLoader } from 'drei'
-import { EffectComposer, Bloom, DepthOfField, Noise, Vignette } from 'react-postprocessing'
+import { EffectComposer, DepthOfField, Vignette } from 'react-postprocessing'
 import bgUrl from './resources/bg.jpg'
 import starsUrl from './resources/stars.png'
 import groundUrl from './resources/ground.png'
 import bearUrl from './resources/bear.png'
 import leaves1Url from './resources/leaves1.png'
 import leaves2Url from './resources/leaves2.png'
+import codeUrl from './resources/code.png'
 import './materials/layerMaterial'
+
+import create from 'zustand'
+import PrismCode from 'react-prism'
+import 'prismjs'
+import 'prismjs/components/prism-jsx.min'
+import 'prismjs/themes/prism-okaidia.css'
+
+const code = `import create from 'zustand'
+
+const useStore = create(set => ({
+  count: 1,
+  inc: () => set(state => ({ count: state.count + 1 })),
+}))
+
+function Controls() {
+  const inc = useStore(state => state.inc)
+  return <button onClick={inc}>one up</button>
+)
+
+function Counter() {
+  const count = useStore(state => state.count)
+  return <h1>{count}</h1>  
+}`
+
+const [useStore] = create(set => ({
+  count: 1,
+  inc: () => set(state => ({ count: state.count + 1 })),
+}))
+
+function Counter() {
+  const { count, inc } = useStore()
+  return (
+    <div class="counter">
+      <span>{count}</span>
+      <button onClick={inc}>one up</button>
+    </div>
+  )
+}
 
 function Scene({ dof }) {
   const scaleN = useAspect('cover', 1600, 1000, 0.21)
   const scaleW = useAspect('cover', 2200, 1000, 0.21)
-  const [bg, stars, ground, bear, leaves1, leaves2] = useTextureLoader([
+  const [bg, stars, ground, bear, leaves1, leaves2, code] = useTextureLoader([
     bgUrl,
     starsUrl,
     groundUrl,
     bearUrl,
     leaves1Url,
     leaves2Url,
+    codeUrl,
   ])
   const subject = useRef()
   const group = useRef()
@@ -32,6 +72,7 @@ function Scene({ dof }) {
     { texture: bg, z: 0, factor: 0.005, scale: scaleW },
     { texture: stars, z: 10, factor: 0.005, scale: scaleW },
     { texture: ground, z: 20, scale: scaleW },
+    { texture: bear, z: 30, ref: subject, scaleFactor: 0.83, scale: scaleN },
     { texture: bear, z: 30, ref: subject, scaleFactor: 0.83, scale: scaleN },
     { texture: leaves1, factor: 0.03, scaleFactor: 1, z: 40, wiggle: 0.24, scale: scaleW },
     { texture: leaves2, factor: 0.04, scaleFactor: 1.3, z: 49, wiggle: 0.3, scale: scaleW },
@@ -68,17 +109,36 @@ function Scene({ dof }) {
 export default function App() {
   const dof = useRef()
   return (
-    <Canvas
-      gl={{ powerPreference: 'high-performance', antialias: false, stencil: false, alpha: false, depth: false }}
-      orthographic
-      camera={{ zoom: 5, position: [0, 0, 200], far: 300, near: 0 }}>
-      <Suspense fallback={null}>
-        <Scene dof={dof} />
-      </Suspense>
-      <EffectComposer multisampling={0}>
-        <DepthOfField ref={dof} bokehScale={4} focalLength={0.1} />
-        <Vignette />
-      </EffectComposer>
-    </Canvas>
+    <>
+      <Canvas
+        gl={{ powerPreference: 'high-performance', antialias: false, stencil: false, alpha: false, depth: false }}
+        orthographic
+        pixelRatio={1}
+        camera={{ zoom: 5, position: [0, 0, 200], far: 300, near: 0 }}>
+        <Suspense fallback={null}>
+          <Scene dof={dof} />
+        </Suspense>
+        <EffectComposer multisampling={0}>
+          <DepthOfField ref={dof} bokehScale={4} focalLength={0.1} />
+          <Vignette />
+        </EffectComposer>
+      </Canvas>
+      <div class="main">
+        <div class="code">
+          <div class="code-container">
+            <PrismCode className="language-jsx" children={code} />
+            <Counter />
+          </div>
+        </div>
+        <a href="https://github.com/drcmda/zustand" class="top-right" children="Github" />
+        <a href="https://twitter.com/0xca0a" class="bottom-right" children="Twitter" />
+        <a
+          href="https://www.instagram.com/tina.henschel/"
+          class="bottom-left"
+          children="Illustrations @ Tina Henschel"
+        />
+        <span class="header-left">Zustand</span>
+      </div>
+    </>
   )
 }
