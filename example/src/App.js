4248c6495ffa97366120b90a096d6d8f0f47e8e7
Paul Henschel <drcmda@gmail.com>
2020-08-17 21:29:38 +0200

    use cra-rewired, sqoosh images, reduce three

--- example/src/App.js
+++ example/src/App.js
@@ -15,28 +15,12 @@ import groundUrl from './resources/ground.png'
 import bearUrl from './resources/bear.png'
 import leaves1Url from './resources/leaves1.png'
 import leaves2Url from './resources/leaves2.png'
+import code from './resources/code'
 import './materials/layerMaterial'
 
-const code = `import create from 'zustand'
-
-const useStore = create(set => ({
+const [useStore] = create((set) => ({
   count: 1,
-  inc: () => set(state => ({ count: state.count + 1 })),
-}))
-
-function Controls() {
-  const inc = useStore(state => state.inc)
-  return <button onClick={inc}>one up</button>
-)
-
-function Counter() {
-  const count = useStore(state => state.count)
-  return <h1>{count}</h1>  
-}`
-
-const [useStore] = create(set => ({
-  count: 1,
-  inc: () => set(state => ({ count: state.count + 1 })),
+  inc: () => set((state) => ({ count: state.count + 1 })),
 }))
 
 function Counter() {
@@ -52,14 +36,7 @@ function Counter() {
 function Scene({ dof }) {
   const scaleN = useAspect('cover', 1600, 1000, 0.21)
   const scaleW = useAspect('cover', 2200, 1000, 0.21)
-  const [bg, stars, ground, bear, leaves1, leaves2] = useTextureLoader([
-    bgUrl,
-    starsUrl,
-    groundUrl,
-    bearUrl,
-    leaves1Url,
-    leaves2Url,
-  ])
+  const textures = useTextureLoader([bgUrl, starsUrl, groundUrl, bearUrl, leaves1Url, leaves2Url])
   const subject = useRef()
   const group = useRef()
   const layersRef = useRef([])
@@ -67,12 +44,12 @@ function Scene({ dof }) {
   const [tempVector] = useState(() => new THREE.Vector3())
   const [focusVector] = useState(() => new THREE.Vector3())
   const layers = [
-    { texture: bg, z: 0, factor: 0.005, scale: scaleW },
-    { texture: stars, z: 10, factor: 0.005, scale: scaleW },
-    { texture: ground, z: 20, scale: scaleW },
-    { texture: bear, z: 30, ref: subject, scaleFactor: 0.83, scale: scaleN },
-    { texture: leaves1, factor: 0.03, scaleFactor: 1, z: 40, wiggle: 0.24, scale: scaleW },
-    { texture: leaves2, factor: 0.04, scaleFactor: 1.3, z: 49, wiggle: 0.3, scale: scaleW },
+    { texture: textures[0], z: 0, factor: 0.005, scale: scaleW },
+    { texture: textures[1], z: 10, factor: 0.005, scale: scaleW },
+    { texture: textures[2], z: 20, scale: scaleW },
+    { texture: textures[3], z: 30, ref: subject, scaleFactor: 0.83, scale: scaleN },
+    { texture: textures[4], factor: 0.03, scaleFactor: 1, z: 40, wiggle: 0.24, scale: scaleW },
+    { texture: textures[5], factor: 0.04, scaleFactor: 1.3, z: 49, wiggle: 0.3, scale: scaleW },
   ]
 
   useFrame((state, delta) => {
@@ -94,7 +71,7 @@ function Scene({ dof }) {
             movementVector={movementVector}
             textr={texture}
             factor={factor}
-            ref={el => (layersRef.current[i] = el)}
+            ref={(el) => (layersRef.current[i] = el)}
             wiggle={wiggle}
             scaleFactor={scaleFactor}
           />
@@ -105,16 +82,12 @@ function Scene({ dof }) {
 }
 
 const Effects = React.forwardRef((props, ref) => {
-  const { viewport } = useThree()
+  const {
+    viewport: { width, height },
+  } = useThree()
   return (
     <EffectComposer multisampling={0}>
-      <DepthOfField
-        ref={ref}
-        bokehScale={4}
-        focalLength={0.1}
-        width={viewport.width / 2}
-        height={viewport.height / 2}
-      />
+      <DepthOfField ref={ref} bokehScale={4} focalLength={0.1} width={width / 2} height={height / 2} />
       <Vignette />
     </EffectComposer>
   )
@@ -125,8 +98,8 @@ export default function App() {
   return (
     <>
       <Canvas
-        gl={{ powerPreference: 'high-performance', antialias: false, stencil: false, alpha: false, depth: false }}
         orthographic
+        gl={{ powerPreference: 'high-performance', antialias: false, stencil: false, alpha: false, depth: false }}
         camera={{ zoom: 5, position: [0, 0, 200], far: 300, near: 0 }}>
         <Suspense fallback={null}>
           <Scene dof={dof} />
--- example/src/index.js
+++ example/src/index.js
@@ -3,9 +3,4 @@ import ReactDOM from 'react-dom'
 import './styles.css'
 import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
