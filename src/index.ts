5e8349ca6d68bf7217a870388536a9147523f11c
Daishi Kato <dai-shi@users.noreply.github.com>
2020-08-18 22:20:59 +0900

    [v4-experimental] use mutable source (#157)
    * feat: new implementation with useMutableSource
    
    * fix: support slice to be null
    
    * [v3-alpha] clean up types
    
    * shave two bytes
    
    * revert type hack for immer
    
    * update size snapshot
    
    * [v3-alpha] unified api
    
    * shave a byte
    
    * v4-alpha
    
    * update size snapshot
    
    * revert skipped test
    
    * fix test for uMS
    
    * fix typo

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
-import { useEffect, useLayoutEffect, useReducer, useRef } from 'react'
+import {
+  useMemo,
+  // @ts-ignore
+  unstable_createMutableSource as createMutableSource,
+  // @ts-ignore
+  unstable_useMutableSource as useMutableSource,
+} from 'react'
+
 export * from './vanilla'
 import createImpl, {
   Destroy,
@@ -11,10 +18,6 @@ import createImpl, {
   Subscribe,
   StoreApi,
 } from './vanilla'
-
-// For server-side rendering: https://github.com/react-spring/zustand/pull/34
-const useIsoLayoutEffect =
-  typeof window === 'undefined' ? useEffect : useLayoutEffect
 
 export interface UseStore<T extends State> {
   (): T
@@ -31,86 +34,53 @@ export default function create<TState extends State>(
   const api: StoreApi<TState> =
     typeof createState === 'function' ? createImpl(createState) : createState
 
+  const source = createMutableSource(api, () => api.getState())
+
+  const FUNCTION_SYMBOL = Symbol()
+  const functionMap = new WeakMap<Function, { [FUNCTION_SYMBOL]: Function }>()
+
   const useStore: any = <StateSlice>(
     selector: StateSelector<TState, StateSlice> = api.getState,
     equalityFn: EqualityChecker<StateSlice> = Object.is
   ) => {
-    const forceUpdate = useReducer((c) => c + 1, 0)[1] as () => void
-    const currentSliceRef = useRef<StateSlice>()
-    const selectorRef = useRef(selector)
-    const equalityFnRef = useRef(equalityFn)
-    const erroredRef = useRef(false)
-
-    if (currentSliceRef.current === undefined) {
-      currentSliceRef.current = selector(api.getState())
-    }
-
-    let newStateSlice: StateSlice | undefined
-    let hasNewStateSlice = false
-
-    // The selector or equalityFn need to be called during the render phase if
-    // they change. We also want legitimate errors to be visible so we re-run
-    // them if they errored in the subscriber.
-    if (
-      selectorRef.current !== selector ||
-      equalityFnRef.current !== equalityFn ||
-      erroredRef.current
-    ) {
-      // Using local variables to avoid mutations in the render phase.
-      newStateSlice = selector(api.getState())
-      hasNewStateSlice = !equalityFn(
-        currentSliceRef.current as StateSlice,
-        newStateSlice
-      )
-    }
-
-    // Syncing changes in useEffect.
-    useIsoLayoutEffect(() => {
-      if (hasNewStateSlice) {
-        currentSliceRef.current = newStateSlice as StateSlice
-      }
-      selectorRef.current = selector
-      equalityFnRef.current = equalityFn
-      erroredRef.current = false
-    })
-
-    useIsoLayoutEffect(() => {
-      const listener = () => {
+    const getSnapshot = useMemo(() => {
+      let lastSlice: StateSlice | undefined
+      return () => {
+        let slice = lastSlice
         try {
-          const nextStateSlice = selectorRef.current(api.getState())
-          if (
-            !equalityFnRef.current(
-              currentSliceRef.current as StateSlice,
-              nextStateSlice
-            )
-          ) {
-            currentSliceRef.current = nextStateSlice
-            forceUpdate()
+          slice = selector(api.getState())
+          if (lastSlice !== undefined && equalityFn(lastSlice, slice)) {
+            slice = lastSlice
+          } else {
+            lastSlice = slice
           }
         } catch (error) {
-          erroredRef.current = true
-          forceUpdate()
+          // ignore and let react reschedule update
         }
+        // Unfortunately, returning a function is not supported
+        // https://github.com/facebook/react/issues/18823
+        if (typeof slice === 'function') {
+          if (functionMap.has(slice)) {
+            return functionMap.get(slice)
+          }
+          const wrappedFunction = { [FUNCTION_SYMBOL]: slice }
+          functionMap.set(slice, wrappedFunction)
+          return wrappedFunction
+        }
+        return slice
       }
-      const unsubscribe = api.subscribe(listener)
-      return unsubscribe
-    }, [])
-
-    return hasNewStateSlice
-      ? (newStateSlice as StateSlice)
-      : currentSliceRef.current
+    }, [selector, equalityFn])
+    const snapshot = useMutableSource(source, getSnapshot, api.subscribe)
+    if (
+      snapshot &&
+      (snapshot as { [FUNCTION_SYMBOL]: unknown })[FUNCTION_SYMBOL]
+    ) {
+      return snapshot[FUNCTION_SYMBOL]
+    }
+    return snapshot
   }
 
   Object.assign(useStore, api)
-
-  // For backward compatibility (No TS types for this)
-  useStore[Symbol.iterator] = function* () {
-    console.warn(
-      '[useStore, api] = create() is deprecated and will be removed in v4'
-    )
-    yield useStore
-    yield api
-  }
 
   return useStore
 }
