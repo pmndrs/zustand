925a90b0d126c3fa711a63e2ef73f63218cbbc07
Daishi Kato <dai-shi@users.noreply.github.com>
2020-09-01 21:45:03 +0900

    fix: devtools to patch api.setState (#172)
    * wip: devtools to patch api.setState
    
    * fix: devtools to patch api.setState
    
    * fix double logging with redux middleware
    
    * fix redux middleware
    
    * fix devtools middleware
    
    * Revert "fix devtools middleware"
    
    This reverts commit 611b696e9d46062582e81cb98e70d133c3aed0e6.
    
    * Revert "fix redux middleware"
    
    This reverts commit 99a9f64e398857ea4fd93def02f9147a1ae065ae.
    
    * chore: code tweaks
    
    * update size snapshot

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,32 +10,36 @@ const redux = <S extends State, A extends { type: unknown }>(
   reducer: (state: S, action: A) => S,
   initial: S
 ) => (
-  set: SetState<S & { dispatch: (a: A) => A }>,
-  get: GetState<S & { dispatch: (a: A) => A }>,
-  api: StoreApi<S & { dispatch: (a: A) => A }> & {
+  set: SetState<S>,
+  get: GetState<S>,
+  api: StoreApi<S> & {
     dispatch?: (a: A) => A
     devtools?: any
   }
 ): S & { dispatch: (a: A) => A } => {
   api.dispatch = (action: A) => {
     set((state: S) => reducer(state, action))
-    api.devtools && api.devtools.send(api.devtools.prefix + action.type, get())
+    if (api.devtools) {
+      api.devtools.send(api.devtools.prefix + action.type, get())
+    }
     return action
   }
   return { dispatch: api.dispatch, ...initial }
 }
 
+type NamedSet<S> = (
+  partial: PartialState<S>,
+  replace?: boolean,
+  name?: string
+) => void
+
 const devtools = <S extends State>(
-  fn: (
-    set: (partial: PartialState<S>, replace?: boolean, name?: string) => void,
-    get: GetState<S>,
-    api: StoreApi<S>
-  ) => S,
+  fn: (set: NamedSet<S>, get: GetState<S>, api: StoreApi<S>) => S,
   prefix?: string
 ) => (
   set: SetState<S>,
   get: GetState<S>,
-  api: StoreApi<S> & { devtools?: any }
+  api: StoreApi<S> & { dispatch?: unknown; devtools?: any }
 ): S => {
   let extension
   try {
@@ -51,18 +55,19 @@ const devtools = <S extends State>(
     api.devtools = null
     return fn(set, get, api)
   }
-  const namedSet = (
-    state: PartialState<S>,
-    replace?: boolean,
-    name?: string | false
-  ) => {
+  const namedSet: NamedSet<S> = (state, replace, name) => {
     set(state, replace)
-    if (name !== false) {
+    if (!api.dispatch) {
       api.devtools.send(api.devtools.prefix + (name || 'action'), get())
     }
   }
   const initialState = fn(namedSet, get, api)
   if (!api.devtools) {
+    const savedSetState = api.setState
+    api.setState = (state: PartialState<S>, replace?: boolean) => {
+      savedSetState(state, replace)
+      api.devtools.send(api.devtools.prefix + 'setState', api.getState())
+    }
     api.devtools = extension.connect({ name: prefix })
     api.devtools.prefix = prefix ? `${prefix} > ` : ''
     api.devtools.subscribe((message: any) => {
@@ -70,11 +75,11 @@ const devtools = <S extends State>(
         const ignoreState =
           message.payload.type === 'JUMP_TO_ACTION' ||
           message.payload.type === 'JUMP_TO_STATE'
-        namedSet(
-          JSON.parse(message.state),
-          false,
-          !initialState.dispatch && !ignoreState && 'setState'
-        )
+        if (!api.dispatch && !ignoreState) {
+          api.setState(JSON.parse(message.state))
+        } else {
+          savedSetState(JSON.parse(message.state))
+        }
       }
     })
     api.devtools.init(initialState)
