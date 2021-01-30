cbef48f00d43f6e475513afd0e5dc52951718f05
Dimas <dimasjkl@gmail.com>
2021-01-30 15:58:34 +0700

    feat(persist): add tests

---
+++ tests/persist.test.tsx
@@ -0,0 +1,90 @@
+import React from 'react'
+import { act, cleanup, render } from '@testing-library/react'
+import create from '../src/index'
+import { persist } from '../src/middleware'
+
+const consoleError = console.error
+afterEach(() => {
+  cleanup()
+  console.error = consoleError
+})
+
+it('can rehydrate state', async () => {
+  let postRehydrationCallbackCallCount = 0
+
+  const useStore = create(
+    persist(
+      () => ({
+        count: 0,
+        name: 'empty',
+      }),
+      {
+        name: 'test-storage',
+        getStorage: () => ({
+          getItem: async (name: string) =>
+            JSON.stringify({
+              state: { count: 42, name },
+              version: 0,
+            }),
+          setItem: () => {},
+        }),
+        onRehydrateStorage: () => (state) => {
+          postRehydrationCallbackCallCount++
+          expect(state.count).toBe(42)
+          expect(state.name).toBe('test-storage')
+        },
+      }
+    )
+  )
+
+  function Counter() {
+    const { count, name } = useStore()
+    return (
+      <div>
+        count: {count}, name: {name}
+      </div>
+    )
+  }
+
+  const { findByText } = render(<Counter />)
+
+  await findByText('count: 0, name: empty')
+  await findByText('count: 42, name: test-storage')
+  expect(postRehydrationCallbackCallCount).toBe(1)
+})
+
+it('can persist state', async () => {
+  let setItemCallCount = 0
+
+  const useStore = create<any>(
+    persist(() => ({ count: 0 }), {
+      name: 'test-storage',
+      getStorage: () => ({
+        getItem: () => null,
+        setItem: (name: string, value: string) => {
+          setItemCallCount++
+          expect(name).toBe('test-storage')
+          expect(value).toBe(
+            JSON.stringify({
+              state: { count: 42 },
+              version: 0,
+            })
+          )
+        },
+      }),
+    })
+  )
+
+  function Counter() {
+    const { count } = useStore()
+    return <div>count: {count}</div>
+  }
+
+  const { findByText } = render(<Counter />)
+
+  await findByText('count: 0')
+  act(() => useStore.setState({ count: 42 }))
+
+  await findByText('count: 42')
+  expect(setItemCallCount).toBe(1)
+})
