47d828db75e51657a550d2998ee69862332847d0
Munawwar <munawwarfiroz@hotmail.com>
2021-04-27 12:36:09 +0400

    1. remove the useEffect we dont need anymore 2. wrap context and hook into createZustand() function, but keep defaults

--- src/context.ts
+++ src/context.ts
@@ -1,51 +1,50 @@
 import React, { createContext, useContext, useRef } from 'react'
 import create, { UseStore } from './index'
 import { EqualityChecker, State, StateCreator, StateSelector } from './vanilla'
-import useIsomorphicLayoutEffect from './useIsomorphicLayoutEffect'
 
-const ZustandContext = createContext<UseStore<State> | undefined>(undefined)
+function createZustand<TState extends State>(initialState?: TState) {
+  const ZustandContext = createContext<UseStore<TState> | undefined>(undefined)
 
-export const Provider = <TState extends State>({
-  createState,
-  children,
-}: {
-  createState: StateCreator<TState>
-  children: React.ReactNode
-}) => {
-  const storeRef = useRef() as React.MutableRefObject<UseStore<TState>>
+  const Provider = ({
+    createState,
+    children,
+  }: {
+    createState: StateCreator<TState>
+    children: React.ReactNode
+  }) => {
+    const storeRef = useRef() as React.MutableRefObject<UseStore<TState>>
 
-  let createdFirstTime = false
-  if (!storeRef.current) {
-    storeRef.current = create(createState)
-    createdFirstTime = true
-  }
-
-  // handle store property change
-  useIsomorphicLayoutEffect(() => {
-    if (!createdFirstTime) {
+    if (!storeRef.current) {
       storeRef.current = create(createState)
     }
-  }, [createState])
 
-  // TODO: I don't like this type assertion
-  const Provider = (ZustandContext.Provider as unknown) as React.Provider<
-    UseStore<TState>
-  >
-  return React.createElement(Provider, { value: storeRef.current }, children)
-}
-
-export const useZustand = <TState extends State, StateSlice>(
-  selector: StateSelector<TState, StateSlice>,
-  eqlFn: EqualityChecker<StateSlice> = Object.is
-) => {
-  // ZustandContext value is guaranteed to be stable.
-  // TODO: I don't like this type assertion
-  const useStore = useContext(ZustandContext) as UseStore<TState> | undefined
-  if (!useStore) {
-    // TODO: decide.. throw? or console.error() and return Error()?
-    throw new Error(
-      'Seems like you have not used zustand provider as an ancestor.'
+    return React.createElement(
+      ZustandContext.Provider,
+      { value: storeRef.current },
+      children
     )
   }
-  return useStore(selector, eqlFn)
+
+  const useZustand = <StateSlice>(
+    selector: StateSelector<TState, StateSlice>,
+    eqlFn: EqualityChecker<StateSlice> = Object.is
+  ) => {
+    // ZustandContext value is guaranteed to be stable.
+    const useStore = useContext(ZustandContext)
+    if (!useStore) {
+      throw new Error(
+        'Seems like you have not used zustand provider as an ancestor.'
+      )
+    }
+    return useStore(selector, eqlFn)
+  }
+
+  return {
+    Provider,
+    useZustand,
+  }
 }
+
+export default createZustand
+
+export const { Provider, useZustand } = createZustand({})
