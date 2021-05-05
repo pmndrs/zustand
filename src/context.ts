611551c40906c4afec88b5f2b95e40004228f3bb
Munawwar <munawwarfiroz@hotmail.com>
2021-05-05 18:54:29 +0400

    #182 SSR data is shared (#375)
    * context utils for SSR
    
    * move useIsomorphicLayoutEffect to new file
    
    * added build commands
    
    * 1. remove the useEffect we dont need anymore 2. wrap context and hook into createZustand() function, but keep defaults
    
    * issue #182 - changed name to createContext and useStore + added tests
    
    * remove default Provider
    
    * use alias to index file
    
    * change 'createState' prop to 'initialStore'
    
    that accepts useStore func/object from create().
    
    This is needed as store access is needed for merging/memoizing, in next.js integration
    
    * updated tests
    
    * code review changes
    
    * snapshot update
    
    * add a section in readme
    
    * chore imports and so on
    
    Co-authored-by: daishi <daishi@axlight.com>

---
+++ src/context.ts
@@ -0,0 +1,59 @@
+import {
+  ReactNode,
+  createElement,
+  createContext as reactCreateContext,
+  useContext,
+  useRef,
+} from 'react'
+import { UseStore } from 'zustand'
+import { EqualityChecker, State, StateSelector } from './vanilla'
+
+function createContext<TState extends State>(_initialState?: TState) {
+  const ZustandContext = reactCreateContext<UseStore<TState> | undefined>(
+    undefined
+  )
+
+  const Provider = ({
+    initialStore,
+    children,
+  }: {
+    initialStore: UseStore<TState>
+    children: ReactNode
+  }) => {
+    const storeRef = useRef<UseStore<TState>>()
+
+    if (!storeRef.current) {
+      storeRef.current = initialStore
+    }
+
+    return createElement(
+      ZustandContext.Provider,
+      { value: storeRef.current },
+      children
+    )
+  }
+
+  const useStore = <StateSlice>(
+    selector?: StateSelector<TState, StateSlice>,
+    equalityFn: EqualityChecker<StateSlice> = Object.is
+  ) => {
+    // ZustandContext value is guaranteed to be stable.
+    const useProviderStore = useContext(ZustandContext)
+    if (!useProviderStore) {
+      throw new Error(
+        'Seems like you have not used zustand provider as an ancestor.'
+      )
+    }
+    return useProviderStore(
+      selector as StateSelector<TState, StateSlice>,
+      equalityFn
+    )
+  }
+
+  return {
+    Provider,
+    useStore,
+  }
+}
+
+export default createContext
---
+++ tests/context.test.tsx
@@ -0,0 +1,92 @@
+import React from 'react'
+import { cleanup, render } from '@testing-library/react'
+import create from '../src/index'
+import createContext from '../src/context'
+
+const consoleError = console.error
+afterEach(() => {
+  cleanup()
+  console.error = consoleError
+})
+
+type CounterState = {
+  count: number
+  inc: () => void
+}
+
+it('creates and uses context store', async () => {
+  const { Provider, useStore } = createContext<CounterState>()
+
+  const store = create<CounterState>((set) => ({
+    count: 0,
+    inc: () => set((state) => ({ count: state.count + 1 })),
+  }))
+
+  function Counter() {
+    const { count, inc } = useStore()
+    React.useEffect(inc, [inc])
+    return <div>count: {count * 1}</div>
+  }
+
+  const { findByText } = render(
+    <Provider initialStore={store}>
+      <Counter />
+    </Provider>
+  )
+
+  await findByText('count: 1')
+})
+
+it('uses context store with selectors', async () => {
+  const { Provider, useStore } = createContext<CounterState>()
+
+  const store = create<CounterState>((set) => ({
+    count: 0,
+    inc: () => set((state) => ({ count: state.count + 1 })),
+  }))
+
+  function Counter() {
+    const count = useStore((state) => state.count)
+    const inc = useStore((state) => state.inc)
+    React.useEffect(inc, [inc])
+    return <div>count: {count * 1}</div>
+  }
+
+  const { findByText } = render(
+    <Provider initialStore={store}>
+      <Counter />
+    </Provider>
+  )
+
+  await findByText('count: 1')
+})
+
+it('throws error when not using provider', async () => {
+  console.error = jest.fn()
+
+  class ErrorBoundary extends React.Component<{}, { hasError: boolean }> {
+    constructor(props: {}) {
+      super(props)
+      this.state = { hasError: false }
+    }
+    static getDerivedStateFromError() {
+      return { hasError: true }
+    }
+    render() {
+      return this.state.hasError ? <div>errored</div> : this.props.children
+    }
+  }
+
+  const { useStore } = createContext<CounterState>()
+  function Component() {
+    useStore()
+    return <div>no error</div>
+  }
+
+  const { findByText } = render(
+    <ErrorBoundary>
+      <Component />
+    </ErrorBoundary>
+  )
+  await findByText('errored')
+})
