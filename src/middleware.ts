ee77b3390384c21fe2ee162b812b0bd3fca60a97
daishi <daishi@axlight.com>
2021-06-05 18:19:41 +0900

    run prettier

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,26 +7,28 @@ import {
   StoreApi,
 } from './vanilla'
 
-export const redux = <S extends State, A extends { type: unknown }>(
-  reducer: (state: S, action: A) => S,
-  initial: S
-) => (
-  set: SetState<S>,
-  get: GetState<S>,
-  api: StoreApi<S> & {
-    dispatch?: (a: A) => A
-    devtools?: any
-  }
-): S & { dispatch: (a: A) => A } => {
-  api.dispatch = (action: A) => {
-    set((state: S) => reducer(state, action))
-    if (api.devtools) {
-      api.devtools.send(api.devtools.prefix + action.type, get())
+export const redux =
+  <S extends State, A extends { type: unknown }>(
+    reducer: (state: S, action: A) => S,
+    initial: S
+  ) =>
+  (
+    set: SetState<S>,
+    get: GetState<S>,
+    api: StoreApi<S> & {
+      dispatch?: (a: A) => A
+      devtools?: any
     }
-    return action
+  ): S & { dispatch: (a: A) => A } => {
+    api.dispatch = (action: A) => {
+      set((state: S) => reducer(state, action))
+      if (api.devtools) {
+        api.devtools.send(api.devtools.prefix + action.type, get())
+      }
+      return action
+    }
+    return { dispatch: api.dispatch, ...initial }
   }
-  return { dispatch: api.dispatch, ...initial }
-}
 
 type NamedSet<T extends State> = {
   <K extends keyof T>(
@@ -36,117 +38,118 @@ type NamedSet<T extends State> = {
   ): void
 }
 
-export const devtools = <S extends State>(
-  fn: (set: NamedSet<S>, get: GetState<S>, api: StoreApi<S>) => S,
-  prefix?: string
-) => (
-  set: SetState<S>,
-  get: GetState<S>,
-  api: StoreApi<S> & { dispatch?: unknown; devtools?: any }
-): S => {
-  let extension
-  try {
-    extension =
-      (window as any).__REDUX_DEVTOOLS_EXTENSION__ ||
-      (window as any).top.__REDUX_DEVTOOLS_EXTENSION__
-  } catch {}
+export const devtools =
+  <S extends State>(
+    fn: (set: NamedSet<S>, get: GetState<S>, api: StoreApi<S>) => S,
+    prefix?: string
+  ) =>
+  (
+    set: SetState<S>,
+    get: GetState<S>,
+    api: StoreApi<S> & { dispatch?: unknown; devtools?: any }
+  ): S => {
+    let extension
+    try {
+      extension =
+        (window as any).__REDUX_DEVTOOLS_EXTENSION__ ||
+        (window as any).top.__REDUX_DEVTOOLS_EXTENSION__
+    } catch {}
 
-  if (!extension) {
-    if (
-      process.env.NODE_ENV === 'development' &&
-      typeof window !== 'undefined'
-    ) {
-      console.warn('Please install/enable Redux devtools extension')
-    }
-    api.devtools = null
-    return fn(set, get, api)
-  }
-  const namedSet: NamedSet<S> = (state, replace, name) => {
-    set(state, replace)
-    if (!api.dispatch) {
-      api.devtools.send(api.devtools.prefix + (name || 'action'), get())
-    }
-  }
-  const initialState = fn(namedSet, get, api)
-  if (!api.devtools) {
-    const savedSetState = api.setState
-    api.setState = <
-      K1 extends keyof S = keyof S,
-      K2 extends keyof S = K1,
-      K3 extends keyof S = K2,
-      K4 extends keyof S = K3
-    >(
-      state: PartialState<S, K1, K2, K3, K4>,
-      replace?: boolean
-    ) => {
-      savedSetState(state, replace)
-      api.devtools.send(api.devtools.prefix + 'setState', api.getState())
-    }
-    api.devtools = extension.connect({ name: prefix })
-    api.devtools.prefix = prefix ? `${prefix} > ` : ''
-    api.devtools.subscribe((message: any) => {
-      if (message.type === 'DISPATCH' && message.state) {
-        const ignoreState =
-          message.payload.type === 'JUMP_TO_ACTION' ||
-          message.payload.type === 'JUMP_TO_STATE'
-        if (!api.dispatch && !ignoreState) {
-          api.setState(JSON.parse(message.state))
-        } else {
-          savedSetState(JSON.parse(message.state))
-        }
-      } else if (
-        message.type === 'DISPATCH' &&
-        message.payload?.type === 'COMMIT'
+    if (!extension) {
+      if (
+        process.env.NODE_ENV === 'development' &&
+        typeof window !== 'undefined'
       ) {
-        api.devtools.init(api.getState())
-      } else if (
-        message.type === 'DISPATCH' &&
-        message.payload?.type === 'IMPORT_STATE'
-      ) {
-        const actions = message.payload.nextLiftedState?.actionsById
-        const computedStates =
-          message.payload.nextLiftedState?.computedStates || []
-
-        computedStates.forEach(
-          ({ state }: { state: PartialState<S> }, index: number) => {
-            const action = actions[index] || api.devtools.prefix + 'setState'
-
-            if (index === 0) {
-              api.devtools.init(state)
-            } else {
-              savedSetState(state)
-              api.devtools.send(action, api.getState())
-            }
-          }
-        )
+        console.warn('Please install/enable Redux devtools extension')
       }
-    })
-    api.devtools.init(initialState)
+      api.devtools = null
+      return fn(set, get, api)
+    }
+    const namedSet: NamedSet<S> = (state, replace, name) => {
+      set(state, replace)
+      if (!api.dispatch) {
+        api.devtools.send(api.devtools.prefix + (name || 'action'), get())
+      }
+    }
+    const initialState = fn(namedSet, get, api)
+    if (!api.devtools) {
+      const savedSetState = api.setState
+      api.setState = <
+        K1 extends keyof S = keyof S,
+        K2 extends keyof S = K1,
+        K3 extends keyof S = K2,
+        K4 extends keyof S = K3
+      >(
+        state: PartialState<S, K1, K2, K3, K4>,
+        replace?: boolean
+      ) => {
+        savedSetState(state, replace)
+        api.devtools.send(api.devtools.prefix + 'setState', api.getState())
+      }
+      api.devtools = extension.connect({ name: prefix })
+      api.devtools.prefix = prefix ? `${prefix} > ` : ''
+      api.devtools.subscribe((message: any) => {
+        if (message.type === 'DISPATCH' && message.state) {
+          const ignoreState =
+            message.payload.type === 'JUMP_TO_ACTION' ||
+            message.payload.type === 'JUMP_TO_STATE'
+          if (!api.dispatch && !ignoreState) {
+            api.setState(JSON.parse(message.state))
+          } else {
+            savedSetState(JSON.parse(message.state))
+          }
+        } else if (
+          message.type === 'DISPATCH' &&
+          message.payload?.type === 'COMMIT'
+        ) {
+          api.devtools.init(api.getState())
+        } else if (
+          message.type === 'DISPATCH' &&
+          message.payload?.type === 'IMPORT_STATE'
+        ) {
+          const actions = message.payload.nextLiftedState?.actionsById
+          const computedStates =
+            message.payload.nextLiftedState?.computedStates || []
+
+          computedStates.forEach(
+            ({ state }: { state: PartialState<S> }, index: number) => {
+              const action = actions[index] || api.devtools.prefix + 'setState'
+
+              if (index === 0) {
+                api.devtools.init(state)
+              } else {
+                savedSetState(state)
+                api.devtools.send(action, api.getState())
+              }
+            }
+          )
+        }
+      })
+      api.devtools.init(initialState)
+    }
+    return initialState
   }
-  return initialState
-}
 
 type Combine<T, U> = Omit<T, keyof U> & U
-export const combine = <
-  PrimaryState extends State,
-  SecondaryState extends State
->(
-  initialState: PrimaryState,
-  create: (
-    set: SetState<PrimaryState>,
-    get: GetState<PrimaryState>,
-    api: StoreApi<PrimaryState>
-  ) => SecondaryState
-): StateCreator<Combine<PrimaryState, SecondaryState>> => (set, get, api) =>
-  Object.assign(
-    {},
-    initialState,
-    create(
-      (set as unknown) as SetState<PrimaryState>,
-      (get as unknown) as GetState<PrimaryState>,
-      (api as unknown) as StoreApi<PrimaryState>
+export const combine =
+  <PrimaryState extends State, SecondaryState extends State>(
+    initialState: PrimaryState,
+    create: (
+      set: SetState<PrimaryState>,
+      get: GetState<PrimaryState>,
+      api: StoreApi<PrimaryState>
+    ) => SecondaryState
+  ): StateCreator<Combine<PrimaryState, SecondaryState>> =>
+  (set, get, api) =>
+    Object.assign(
+      {},
+      initialState,
+      create(
+        set as unknown as SetState<PrimaryState>,
+        get as unknown as GetState<PrimaryState>,
+        api as unknown as StoreApi<PrimaryState>
+      )
     )
-  )
 
 export type StateStorage = {
   getItem: (name: string) => string | null | Promise<string | null>
@@ -213,162 +216,163 @@ interface Thenable<Value> {
   ): Thenable<V>
 }
 
-const toThenable = <Result, Input>(
-  fn: (input: Input) => Result | Promise<Result> | Thenable<Result>
-) => (input: Input): Thenable<Result> => {
-  try {
-    const result = fn(input)
-    if (result instanceof Promise) {
-      return result as Thenable<Result>
-    }
-    return {
-      then(onFulfilled) {
-        return toThenable(onFulfilled)(result as Result)
-      },
-      catch(_onRejected) {
-        return this as Thenable<any>
-      },
-    }
-  } catch (e) {
-    return {
-      then(_onFulfilled) {
-        return this as Thenable<any>
-      },
-      catch(onRejected) {
-        return toThenable(onRejected)(e)
-      },
+const toThenable =
+  <Result, Input>(
+    fn: (input: Input) => Result | Promise<Result> | Thenable<Result>
+  ) =>
+  (input: Input): Thenable<Result> => {
+    try {
+      const result = fn(input)
+      if (result instanceof Promise) {
+        return result as Thenable<Result>
+      }
+      return {
+        then(onFulfilled) {
+          return toThenable(onFulfilled)(result as Result)
+        },
+        catch(_onRejected) {
+          return this as Thenable<any>
+        },
+      }
+    } catch (e) {
+      return {
+        then(_onFulfilled) {
+          return this as Thenable<any>
+        },
+        catch(onRejected) {
+          return toThenable(onRejected)(e)
+        },
+      }
     }
   }
-}
 
-export const persist = <S extends State>(
-  config: StateCreator<S>,
-  options: PersistOptions<S>
-) => (set: SetState<S>, get: GetState<S>, api: StoreApi<S>): S => {
-  const {
-    name,
-    getStorage = () => localStorage,
-    serialize = JSON.stringify as (state: StorageValue<S>) => string,
-    deserialize = JSON.parse as (str: string) => StorageValue<S>,
-    blacklist,
-    whitelist,
-    onRehydrateStorage,
-    version = 0,
-    migrate,
-  } = options || {}
+export const persist =
+  <S extends State>(config: StateCreator<S>, options: PersistOptions<S>) =>
+  (set: SetState<S>, get: GetState<S>, api: StoreApi<S>): S => {
+    const {
+      name,
+      getStorage = () => localStorage,
+      serialize = JSON.stringify as (state: StorageValue<S>) => string,
+      deserialize = JSON.parse as (str: string) => StorageValue<S>,
+      blacklist,
+      whitelist,
+      onRehydrateStorage,
+      version = 0,
+      migrate,
+    } = options || {}
 
-  let storage: StateStorage | undefined
+    let storage: StateStorage | undefined
 
-  try {
-    storage = getStorage()
-  } catch (e) {
-    // prevent error if the storage is not defined (e.g. when server side rendering a page)
-  }
+    try {
+      storage = getStorage()
+    } catch (e) {
+      // prevent error if the storage is not defined (e.g. when server side rendering a page)
+    }
 
-  if (!storage) {
-    return config(
-      (...args) => {
-        console.warn(
-          `Persist middleware: unable to update ${name}, the given storage is currently unavailable.`
+    if (!storage) {
+      return config(
+        (...args) => {
+          console.warn(
+            `Persist middleware: unable to update ${name}, the given storage is currently unavailable.`
+          )
+          set(...args)
+        },
+        get,
+        api
+      )
+    }
+
+    const thenableSerialize = toThenable(serialize)
+
+    const setItem = (): Thenable<void> => {
+      const state = { ...get() }
+
+      if (whitelist) {
+        ;(Object.keys(state) as (keyof S)[]).forEach((key) => {
+          !whitelist.includes(key) && delete state[key]
+        })
+      }
+      if (blacklist) {
+        blacklist.forEach((key) => delete state[key])
+      }
+
+      let errorInSync: Error | undefined
+      const thenable = thenableSerialize({ state, version })
+        .then((serializedValue) =>
+          (storage as StateStorage).setItem(name, serializedValue)
         )
+        .catch((e) => {
+          errorInSync = e
+        })
+      if (errorInSync) {
+        throw errorInSync
+      }
+      return thenable
+    }
+
+    const savedSetState = api.setState
+
+    api.setState = (state, replace) => {
+      savedSetState(state, replace)
+      void setItem()
+    }
+
+    // rehydrate initial state with existing stored state
+
+    // a workaround to solve the issue of not storing rehydrated state in sync storage
+    // the set(state) value would be later overridden with initial state by create()
+    // to avoid this, we merge the state from localStorage into the initial state.
+    let stateFromStorageInSync: S | undefined
+    const postRehydrationCallback = onRehydrateStorage?.(get()) || undefined
+    // bind is used to avoid `TypeError: Illegal invocation` error
+    toThenable(storage.getItem.bind(storage))(name)
+      .then((storageValue) => {
+        if (storageValue) {
+          return deserialize(storageValue)
+        }
+      })
+      .then((deserializedStorageValue) => {
+        if (deserializedStorageValue) {
+          if (deserializedStorageValue.version !== version) {
+            if (migrate) {
+              return migrate(
+                deserializedStorageValue.state,
+                deserializedStorageValue.version
+              )
+            }
+            console.error(
+              `State loaded from storage couldn't be migrated since no migrate function was provided`
+            )
+          } else {
+            stateFromStorageInSync = deserializedStorageValue.state
+            set(deserializedStorageValue.state)
+          }
+        }
+      })
+      .then((migratedState) => {
+        if (migratedState) {
+          stateFromStorageInSync = migratedState as S
+          set(migratedState as PartialState<S, keyof S>)
+          return setItem()
+        }
+      })
+      .then(() => {
+        postRehydrationCallback?.(get(), undefined)
+      })
+      .catch((e: Error) => {
+        postRehydrationCallback?.(undefined, e)
+      })
+
+    const configResult = config(
+      (...args) => {
         set(...args)
+        void setItem()
       },
       get,
       api
     )
+
+    return stateFromStorageInSync
+      ? { ...configResult, ...stateFromStorageInSync }
+      : configResult
   }
-
-  const thenableSerialize = toThenable(serialize)
-
-  const setItem = (): Thenable<void> => {
-    const state = { ...get() }
-
-    if (whitelist) {
-      ;(Object.keys(state) as (keyof S)[]).forEach((key) => {
-        !whitelist.includes(key) && delete state[key]
-      })
-    }
-    if (blacklist) {
-      blacklist.forEach((key) => delete state[key])
-    }
-
-    let errorInSync: Error | undefined
-    const thenable = thenableSerialize({ state, version })
-      .then((serializedValue) =>
-        (storage as StateStorage).setItem(name, serializedValue)
-      )
-      .catch((e) => {
-        errorInSync = e
-      })
-    if (errorInSync) {
-      throw errorInSync
-    }
-    return thenable
-  }
-
-  const savedSetState = api.setState
-
-  api.setState = (state, replace) => {
-    savedSetState(state, replace)
-    void setItem()
-  }
-
-  // rehydrate initial state with existing stored state
-
-  // a workaround to solve the issue of not storing rehydrated state in sync storage
-  // the set(state) value would be later overridden with initial state by create()
-  // to avoid this, we merge the state from localStorage into the initial state.
-  let stateFromStorageInSync: S | undefined
-  const postRehydrationCallback = onRehydrateStorage?.(get()) || undefined
-  // bind is used to avoid `TypeError: Illegal invocation` error
-  toThenable(storage.getItem.bind(storage))(name)
-    .then((storageValue) => {
-      if (storageValue) {
-        return deserialize(storageValue)
-      }
-    })
-    .then((deserializedStorageValue) => {
-      if (deserializedStorageValue) {
-        if (deserializedStorageValue.version !== version) {
-          if (migrate) {
-            return migrate(
-              deserializedStorageValue.state,
-              deserializedStorageValue.version
-            )
-          }
-          console.error(
-            `State loaded from storage couldn't be migrated since no migrate function was provided`
-          )
-        } else {
-          stateFromStorageInSync = deserializedStorageValue.state
-          set(deserializedStorageValue.state)
-        }
-      }
-    })
-    .then((migratedState) => {
-      if (migratedState) {
-        stateFromStorageInSync = migratedState as S
-        set(migratedState as PartialState<S, keyof S>)
-        return setItem()
-      }
-    })
-    .then(() => {
-      postRehydrationCallback?.(get(), undefined)
-    })
-    .catch((e: Error) => {
-      postRehydrationCallback?.(undefined, e)
-    })
-
-  const configResult = config(
-    (...args) => {
-      set(...args)
-      void setItem()
-    },
-    get,
-    api
-  )
-
-  return stateFromStorageInSync
-    ? { ...configResult, ...stateFromStorageInSync }
-    : configResult
-}
