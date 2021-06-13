eb0a93b45d4e384533b4ef4237787745992aba3d
Daishi Kato <dai-shi@users.noreply.github.com>
2021-06-13 21:01:29 +0900

    chore: refactor zustand/context (#418)

--- src/context.ts
+++ src/context.ts
@@ -19,12 +19,8 @@ function createContext<TState extends State>() {
     initialStore: UseStore<TState>
     children: ReactNode
   }) => {
-    const storeRef = useRef<UseStore<TState>>()
-
-    if (!storeRef.current) {
-      storeRef.current = initialStore
-    }
-
+    // Put store (hook) in ref to make it stable
+    const storeRef = useRef(initialStore)
     return createElement(
       ZustandContext.Provider,
       { value: storeRef.current },
