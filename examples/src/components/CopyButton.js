d2f5af3391863053244c8933a8198e9afe9dd5fe
Reaper <sidgroks@gmail.com>
2021-07-22 15:35:14 +0530

    refactor(example/site): prism renderer, copy button (#503)
    * refactor(example): prism renderer, copy button
    
    added a more maintained prism renderer instead
    of react-prism and also a copy button in case
    someone wishes to clone the code instead of
    enabling pointer events on everything
    
    * fix(code-preview): wait on mount
    
    wait for the element to mount before showing the preview, fixes
    https://github.com/pmndrs/zustand/pull/503#issuecomment-884536093
    
    * fix(code-preview): replace with `pre` tag
    
    renderer depends on a pre tag, changed style and tags to match the same

---
+++ examples/src/components/CopyButton.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import { copyToClipboard } from '../utils/copy-to-clipboard'
+
+/* 
+Isolated logic for the entire copy functionality instead 
+of a separate button component and with the added utility
+*/
+export function CopyButton({ code, ...props }) {
+  const [isCopied, setIsCopied] = useState(false)
+
+  const handleCopy = () => {
+    copyToClipboard(code)
+    setIsCopied(true)
+    setTimeout(() => setIsCopied(false), 3000)
+  }
+
+  return (
+    <>
+      <button className="copy-button" onClick={handleCopy} {...props}>
+        {isCopied ? (
+          'Copied!'
+        ) : (
+          <>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={16}
+              height={16}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              {...props}>
+              <rect x={9} y={9} width={13} height={13} rx={2} ry={2} />
+              <path d="M5 15H4a2 2 0 01-2-2V4a2 2 0 012-2h9a2 2 0 012 2v1" />
+            </svg>
+          </>
+        )}
+      </button>
+    </>
+  )
+}
