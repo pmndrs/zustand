d2800e861f9e1908d204aae371135c74946e8f1b
Siddharth <ahoy@barelyhuman.dev>
2021-08-16 10:12:56 +0530

    refactor: change config to resolve to single folder

--- rollup.config.js
+++ rollup.config.js
@@ -27,6 +27,8 @@ function getEsbuild(target) {
     minify: false,
     target,
     tsconfig: path.resolve('./tsconfig.json'),
+    allowSyntheticDefaultImports: true,
+    module: 'esnext',
   })
 }
 
@@ -37,7 +39,13 @@ function createDeclarationConfig(input, output) {
       dir: output,
     },
     external,
-    plugins: [typescript({ declaration: true, outDir: output })],
+    plugins: [
+      typescript({
+        declaration: true,
+        emitDeclarationOnly: true,
+        outDir: output,
+      }),
+    ],
   }
 }
 
@@ -69,12 +77,12 @@ export default function (args) {
     c = c.slice('config-'.length)
     return [
       createCommonJSConfig(`src/${c}.ts`, `dist/${c}.js`),
-      createESMConfig(`src/${c}.ts`, `dist/esm/${c}.js`),
+      createESMConfig(`src/${c}.ts`, `dist/${c}.mjs`),
     ]
   }
   return [
     createDeclarationConfig('src/index.ts', 'dist'),
     createCommonJSConfig('src/index.ts', 'dist/index.js'),
-    createESMConfig('src/index.ts', 'dist/esm/index.js'),
+    createESMConfig('src/index.ts', 'dist/index.mjs'),
   ]
 }
