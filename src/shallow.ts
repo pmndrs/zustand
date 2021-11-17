796cf005fd580a6e1aa9d2e89c86bce68cf9d2a9
Devansh Jethmalani <jethmalani.devansh@gmail.com>
2021-11-17 15:50:13 +0530

    rewrite a less strict `zustand/shallow`

--- src/shallow.ts
+++ src/shallow.ts
@@ -1,26 +1,37 @@
-export default function shallow<T, U>(objA: T, objB: U) {
-  if (Object.is(objA, objB)) {
-    return true
+const shallow = <A, B>(_a: A, _b: B) => {
+  if (is(_a, _b)) return true
+
+  if (typeof _a !== 'object') return false
+  if (typeof _b !== 'object') return false
+  if (_a === null) return false
+  if (_b === null) return false
+
+  let a = _a as WidenTo<A, B>;
+  let b = _b as WidenTo<B, A>;
+  let aKeys = keys(a)
+  let bKeys = keys(b)
+  let checkedKeys = new Set<keyof A | keyof B>();
+  
+  for (let k of aKeys) {
+    if (!is(a[k], b[k])) return false;
+    checkedKeys.add(k)
   }
-  if (
-    typeof objA !== 'object' ||
-    objA === null ||
-    typeof objB !== 'object' ||
-    objB === null
-  ) {
-    return false
+
+  for (let k of bKeys) {
+    if (checkedKeys.has(k)) continue;
+    if (!is(b[k], a[k])) return false;
   }
-  const keysA = Object.keys(objA)
-  if (keysA.length !== Object.keys(objB).length) {
-    return false
-  }
-  for (let i = 0; i < keysA.length; i++) {
-    if (
-      !Object.prototype.hasOwnProperty.call(objB, keysA[i] as string) ||
-      !Object.is(objA[keysA[i] as keyof T], objB[keysA[i] as keyof U])
-    ) {
-      return false
-    }
-  }
+
   return true
 }
+
+const is =
+  Object.is as (a: unknown, b: unknown) => boolean;
+
+const keys =
+  Object.keys as <T>(t: T) => (keyof T & string)[];
+
+type WidenTo<T, U> =
+  T & { [K in keyof U]?: never }
+
+export default shallow;
