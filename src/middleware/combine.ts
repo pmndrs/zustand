12df218d0005990413f0947d5561253eace07a1e
Devansh Jethmalani <jethmalani.devansh@gmail.com>
2021-11-17 15:17:32 +0530

    rewrite `combine`

--- src/middleware/combine.ts
+++ src/middleware/combine.ts
@@ -1,25 +1,64 @@
-import { GetState, SetState, State, StoreApi } from '../vanilla'
-import { NamedSet } from './devtools'
+import { Store, UnknownState, StoreInitializer } from '../vanilla'
 
-type Combine<T, U> = Omit<T, keyof U> & U
+// ============================================================================
+// Types
 
-export const combine =
-  <PrimaryState extends State, SecondaryState extends State>(
-    initialState: PrimaryState,
-    create: (
-      // Note: NamedSet added for convenience
-      set: SetState<PrimaryState> & NamedSet<PrimaryState>,
-      get: GetState<PrimaryState>,
-      api: StoreApi<PrimaryState>
-    ) => SecondaryState
-  ) =>
-  (
-    set: SetState<Combine<PrimaryState, SecondaryState>>,
-    get: GetState<Combine<PrimaryState, SecondaryState>>,
-    api: StoreApi<Combine<PrimaryState, SecondaryState>>
-  ) =>
-    Object.assign(
-      {},
-      initialState,
-      create(set as any, get as any, api as any)
-    ) as Combine<PrimaryState, SecondaryState>
+type Combine =
+  <T extends UnknownState, U extends UnknownState>
+    ( initialState: T
+    , additionalStateCreator:
+        ( set: Store<T>['setState']
+        , get: Store<O.Overwrite<T, U>>['getState']
+        , store:
+            O.Overwrite<
+              Store<O.Overwrite<T, U>>,
+              { setState: Store<T>['setState'] }
+            >
+        ) => U
+    ) =>
+      StoreInitializer<O.Overwrite<T, U>, Store<O.Overwrite<T, U>>>
+
+
+// ============================================================================
+// Implementation
+
+const combine: Combine = (initialState, additionalStateCreator) => (...a) =>
+  overwrite(
+    initialState,
+    additionalStateCreator(...a)
+  )
+
+
+// ============================================================================
+// Utilities
+
+const overwrite = <T extends O.Unknown, U extends O.Unknown>(t: T, u: U) =>
+  Object.assign({}, t, u) as O.Overwrite<T, U>
+
+namespace O {
+  export type Unknown =
+    object
+
+  export type Overwrite<T extends O.Unknown, U extends O.Unknown> =
+    & ExcludeKey<T, U.Extract<keyof U, keyof T>>
+    & U
+
+  export type ExcludeKey<T extends O.Unknown, K extends keyof T> =
+    { [P in U.Exclude<keyof T, K>]?:
+        T[P]
+    }
+}
+
+namespace U {
+  export type Exclude<T, U> =
+    T extends U ? never : U
+
+  export type Extract<T, U> =
+    T extends U ? T : never
+}
+
+
+// ============================================================================
+// Exports
+
+export { combine }
