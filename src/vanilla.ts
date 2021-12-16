b5be4ace98bd116a801afe45022fc6cbca5224ee
Devansh Jethmalani <jethmalani.devansh@gmail.com>
2021-12-17 03:37:37 +0530

    type deprecations

--- src/vanilla.ts
+++ src/vanilla.ts
@@ -1,6 +1,18 @@
+/**
+ * @deprecated `State` is renamed to `UnknownState`,
+ * `State` will be removed in next major
+ */
 export type State = object
+
+export type UnknownState = object;
+
 // types inspired by setState from React, see:
 // https://github.com/DefinitelyTyped/DefinitelyTyped/blob/6c49e45842358ba59a508e13130791989911430d/types/react/v16/index.d.ts#L489-L495
+/**
+ * @deprecated Use the builtin `Partial<T>` instead of `PartialState<T>`.
+ * Additionally turn on `--exactOptionalPropertyTypes` tsc flag.
+ * `PartialState` will be removed in next major
+ */
 export type PartialState<
   T extends State,
   K1 extends keyof T = keyof T,
@@ -10,10 +22,35 @@ export type PartialState<
 > =
   | (Pick<T, K1> | Pick<T, K2> | Pick<T, K3> | Pick<T, K4> | T)
   | ((state: T) => Pick<T, K1> | Pick<T, K2> | Pick<T, K3> | Pick<T, K4> | T)
+
+/**
+ * @deprecated Use `(t: T) => U` instead of `StateSelector<T, U>`.
+ * `StateSelector` will be removed in next major.
+ */
 export type StateSelector<T extends State, U> = (state: T) => U
+
+/**
+ * @deprecated Use `(a: T, b: T) => boolean` instead of `EqualityChecker<T>.
+ * `EqualityChecker` will be removed in next major.
+ */
 export type EqualityChecker<T> = (state: T, newState: T) => boolean
+
+/** 
+ * @deprecated Use `(state: T, prevState: T) => void` instead of `StateListener<T>`.
+ * `StateListener` will be removed in next major.
+ */
 export type StateListener<T> = (state: T, previousState: T) => void
+
+/** 
+ * @deprecated Use `(slice: T, prevSlice: T) => void` instead of `StateSliceListener<T>`.
+ * `StateSliceListener` will be removed in next major.
+ */
 export type StateSliceListener<T> = (slice: T, previousSlice: T) => void
+
+/**
+ * @deprecated Use `Store<T>['subscribe']` instead of `Subscribe<T>`.
+ * `Subscribe` will be removed in next major.
+ */
 export type Subscribe<T extends State> = {
   (listener: StateListener<T>): () => void
   /**
@@ -26,6 +63,10 @@ export type Subscribe<T extends State> = {
   ): () => void
 }
 
+/**
+ * @deprecated Use `Store<T>['setState']` instead of `SetState<T>`
+ * `SetState` will be removed in next major.
+ */
 export type SetState<T extends State> = {
   <
     K1 extends keyof T,
@@ -37,15 +78,50 @@ export type SetState<T extends State> = {
     replace?: boolean
   ): void
 }
+
+/**
+ * @deprecated Use `() => T` or `Store<T>['getState']` instead of `GetState<T>`
+ * `GetState` will be removed in next major.
+ */
 export type GetState<T extends State> = () => T
+
+/**
+ * @deprecated Use `() => void` or `Store<UnknownState>['destroy']` instead of `Destroy`.
+ * `Destroy` will be removed in next major.
+ */
 export type Destroy = () => void
+
+/**
+ * @deprecated `StoreApi<T>` has been renamed to `Store<T>`
+ * `StoreApi` will be removed in next major.
+ */
 export type StoreApi<T extends State> = {
   setState: SetState<T>
   getState: GetState<T>
   subscribe: Subscribe<T>
   destroy: Destroy
 }
+
+export type Store<T extends State> = {
+  setState: SetState<T>
+  getState: GetState<T>
+  subscribe: Subscribe<T>
+  destroy: Destroy
+}
+
+/**
+ * @deprecated `StateCreator` has been renamed to `StoreInitializer`.
+ * `StateCreator` will be removed in next major.
+ */
 export type StateCreator<
+  T extends State,
+  CustomSetState = SetState<T>,
+  CustomGetState = GetState<T>,
+  CustomStoreApi extends StoreApi<T> = StoreApi<T>
+> = (set: CustomSetState, get: CustomGetState, api: CustomStoreApi) => T
+
+
+export type StoreInitializer<
   T extends State,
   CustomSetState = SetState<T>,
   CustomGetState = GetState<T>,
