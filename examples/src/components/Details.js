258e2ddd8ec1e2973b220ae4efcbf3b76ed4927c
Daishi Kato <dai-shi@users.noreply.github.com>
2022-01-13 12:19:33 +0900

    Update examples/src/components/Details.js

--- examples/src/components/Details.js
+++ examples/src/components/Details.js
@@ -5,7 +5,7 @@ export default function () {
     <>
       <a href="https://github.com/pmndrs/zustand" className="top-right" children="Github" />
       <div className="bottom">
-        <a href="https://codesandbox.io/s/xgjtc" className="bottom-right" children="<Source />" />
+        <a href="https://github.com/pmndrs/zustand/tree/main/examples" className="bottom-right" children="<Source />" />
         <a
           href="https://www.instagram.com/tina.henschel/"
           className="bottom-left"
