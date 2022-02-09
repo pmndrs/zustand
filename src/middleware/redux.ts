33b5fd35461e194fbc308a722864f817e0a64d36
Devansh Jethmalani <jethmalani.devansh@gmail.com>
2022-02-09 18:41:09 +0530

    redux: deprecate `StoreApiWithRedux`

--- src/middleware/redux.ts
+++ src/middleware/redux.ts
@@ -10,6 +10,10 @@ type DevtoolsType = {
   error: (payload: any) => void
 }
 
+/**
+ * @deprecated Use `Mutate<StoreApi<T & { dispatch: (a: A) => A }>, [["zustand/redux", A]]>`.
+ * If you have multiple middlewares see the documentation for `Mutate` usage.
+ */
 export type StoreApiWithRedux<
   T extends State,
   A extends { type: unknown }
@@ -17,6 +21,26 @@ export type StoreApiWithRedux<
   dispatch: (a: A) => A
   dispatchFromDevtools: boolean
 }
+
+declare module '../vanilla' {
+  interface StoreMutators<S, A> {
+    'zustand/redux': WithRedux<S, A>
+  }
+}
+
+type WithRedux<S, A> = Write<Cast<S, object>, StoreRedux<Cast<A, Action>>>
+
+interface Action {
+  type: unknown
+}
+
+interface StoreRedux<A extends Action> {
+  dispatch: (a: A) => A
+  dispatchFromDevtools: true
+}
+
+type Write<T extends object, U extends object> = Omit<T, keyof U> & U
+type Cast<T, U> = T extends U ? T : U
 
 export const redux =
   <S extends State, A extends { type: unknown }>(
