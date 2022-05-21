d320629993ef63ac151ccac26416427369c50ff1
Alek Angelov <alek.angelov.sk@gmail.com>
2022-05-22 01:52:30 +0200

    memory footprint

--- src/setup-tests.ts
+++ src/setup-tests.ts
@@ -2,8 +2,10 @@ import { act } from '@testing-library/react'
 import type { State, StateCreator, StoreMutatorIdentifier } from './vanilla'
 import actualCreate from './'
 
-// a variable to hold reset functions for all stores declared in the app
-const storeResetFns = new Set<VoidFunction>()
+// maps use more memory than sets, but we can use them for
+// easier memory management via disposing (edge case, but worth noting)
+// not many people are going to destroy stores anyways, but what the heck
+const pool = new Map<string, VoidFunction>()
 
 // when creating a store, we get its initial state, create a reset function and add it in the set
 const create = <
@@ -12,15 +14,30 @@ const create = <
 >(
   createState: StateCreator<T, [], Mos>
 ) => {
+  // make a random id for each of the stores
+  const uid = Math.random().toString(36).substring(2, 12)
+  // create the store
   const store = actualCreate(createState)
+  // on destroy call the og destroy function
+  // and remove it from the pool
+  store.destroy = () => {
+    store.destroy()
+    pool.delete(uid)
+  }
+  // initial state for reset purposes
   const initialState = store.getState()
-  storeResetFns.add(() => store.setState(initialState, true))
+  // add the reset fn to the pool
+  pool.set(uid, () => {
+    store.setState(initialState)
+  })
   return store
 }
 
 // Reset all stores after each test run
 afterEach(() => {
-  act(() => storeResetFns.forEach((resetFn) => resetFn()))
+  act(() => {
+    pool.forEach((fn) => fn())
+  })
 })
 
 jest.mock('./', () => create)
