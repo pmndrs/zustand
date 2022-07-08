44d06582d80f33e72021df13a38291dccc6176fd
Mike <yum650350@gmail.com>
2022-07-08 23:54:39 +0800

    style: fix eslint warning in Details.js

--- examples/src/components/Details.js
+++ examples/src/components/Details.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function () {
+export default function Details() {
   return (
     <>
       <a href="https://github.com/pmndrs/zustand" className="top-right" children="Github" />
