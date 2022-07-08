e705ff0221e95605578afb3a047ed4ccaab8584c
Mike <mike@verybuy.tw>
2022-07-08 14:33:51 +0800

    fix: update react-dom usage

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import './styles.css'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = createRoot(document.getElementById('root'))
+
+root.render(<App />)
