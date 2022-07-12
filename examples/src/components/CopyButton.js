a8f4382bf1bd2f2dc5d780e703ffdfd3b4d20d7e
tbor00 <btimoteo@outlook.com>
2022-07-12 19:11:07 -0300

    fix(copyButton): clearTimeout and useCallback was added on CopyButton

--- examples/src/components/CopyButton.js
+++ examples/src/components/CopyButton.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useCallback, useRef } from 'react'
 import { copyToClipboard } from '../utils/copy-to-clipboard'
 
 /* 
@@ -7,13 +7,17 @@ of a separate button component and with the added utility
 */
 export default function CopyButton({ code, ...props }) {
   const [isCopied, setIsCopied] = useState(false)
+  const timer = useRef()
 
-  const handleCopy = () => {
+  const handleCopy =  useCallback(()  => {
     copyToClipboard(code).then(() => {
       setIsCopied(true)
-      setTimeout(() => setIsCopied(false), 3000)
+      timer.current =  setTimeout(() => setIsCopied(false), 3000)
     })
-  }
+    return () => {
+      clearTimeout(timer.current)
+    }
+  }, [code])
   
 
   return (
