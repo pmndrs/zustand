5a8ae6d53c08a186f311d8f276f4678a3cb1ffd9
Mike <yum650350@gmail.com>
2022-07-13 20:42:20 +0800

    build: Remove the tree shaking for package three
    Removing it because the code in config-overrides is causing error
    on yarn start, will put it back later if tree shaking is still
    necessary, maybe with anothor approach.

--- examples/config-overrides.js
+++ examples/config-overrides.js
@@ -1,11 +1,6 @@
-const { addWebpackPlugin, addWebpackAlias, override } = require('customize-cra')
+const { override } = require('customize-cra')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-const path = require('path')
 
 module.exports = override(
-  addWebpackAlias({
-    three$: path.resolve('./src/utils/three.js'),
-    '../../../build/three.module.js': path.resolve('./src/utils/three.js'),
-  }),
   //addWebpackPlugin(new BundleAnalyzerPlugin())
 )
