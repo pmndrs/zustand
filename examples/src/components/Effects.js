054c9034ebbd148f8bfbeadb725bce28db41e331
Mike <yum650350@gmail.com>
2022-07-14 22:13:54 +0800

    refactor: replace deprecated props of DepthOfField
    Props with and height has been renamed to resolutionX and resolutionY
    ref: https://github.com/pmndrs/postprocessing/releases/tag/v6.27.0

--- examples/src/components/Effects.js
+++ examples/src/components/Effects.js
@@ -8,7 +8,7 @@ const Effects = forwardRef((props, ref) => {
   } = useThree()
   return (
     <EffectComposer multisampling={0}>
-      <DepthOfField ref={ref} bokehScale={4} focalLength={0.1} width={width / 2} height={height / 2} />
+      <DepthOfField ref={ref} bokehScale={4} focalLength={0.1} resolutionX={width / 2} resolutionY={height / 2} />
       <Vignette />
     </EffectComposer>
   )
