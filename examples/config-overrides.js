785e03411df5d39e8260761c8f481d88ca88c16e
Mike <yum650350@gmail.com>
2022-07-19 06:48:37 +0800

    perf(example): Reduce bundle size (#1096)
    * build(script): add `build:analyze` script
    
    * build: bump version for package `webpack-bundle-analyzer`
    
    * build: add `@react-three/babel`
    
    * perf: render `three` objects with custom canvas
    
    * fix: resize for canvas
    
    * fix: extend `PlaneBufferGeometry` for drei's Plane
    
    * build: use prism theme from `prism-react-renderer` instead

--- examples/config-overrides.js
+++ examples/config-overrides.js
@@ -1,6 +1,9 @@
-const { override } = require('customize-cra')
+const { addWebpackPlugin, override, addBabelPlugin } = require('customize-cra')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
-module.exports = override(
-  //addWebpackPlugin(new BundleAnalyzerPlugin())
-)
+const overrides = [
+  addBabelPlugin('module:@react-three/babel'),
+  process.env.BUNDLE_ANALYZE === 'true' ? addWebpackPlugin(new BundleAnalyzerPlugin()) : undefined,
+]
+
+module.exports = override(...overrides.filter((config) => config))
--- examples/src/index.js
+++ examples/src/index.js
@@ -1,7 +1,24 @@
 import { createRoot } from 'react-dom/client'
+import { createRoot as createCanvasRoot, events } from '@react-three/fiber'
 import './styles.css'
 import App from './App'
+import Backdrop from './components/Backdrop'
 
 const root = createRoot(document.getElementById('root'))
 
 root.render(<App />)
+
+const backdrop = createCanvasRoot(document.getElementById('backdrop'))
+
+backdrop
+  .configure({
+    events,
+    orthographic: true,
+    gl: { powerPreference: 'high-performance', antialias: false, stencil: false, alpha: false, depth: false },
+    camera: { zoom: 5, position: [0, 0, 200], far: 300, near: 0 },
+  })
+  .render(<Backdrop />)
+
+window.addEventListener('resize', () => {
+  backdrop.configure({ width: window.innerWidth, height: window.innerHeight })
+})
