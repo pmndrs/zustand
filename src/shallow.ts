ed526d074b12ad11274850532d406e0305a1e60f
Anton <antonylunev@gmail.com>
2022-12-05 04:28:28 -0800

    feat(shallow): Add support of Maps and Sets to shallow comparator (#1451)
    * Add support of Maps and Sets to shallow comparator.
    
    * Update src/shallow.ts
    
    Co-authored-by: Daishi Kato <dai-shi@users.noreply.github.com>
    
    Co-authored-by: Daishi Kato <dai-shi@users.noreply.github.com>

--- src/shallow.ts
+++ src/shallow.ts
@@ -10,6 +10,29 @@ function shallow<T>(objA: T, objB: T) {
   ) {
     return false
   }
+
+  if (objA instanceof Map && objB instanceof Map) {
+    if (objA.size !== objB.size) return false
+
+    for (const [key, value] of objA) {
+      if (!Object.is(value, objB.get(key))) {
+        return false
+      }
+    }
+    return true
+  }
+
+  if (objA instanceof Set && objB instanceof Set) {
+    if (objA.size !== objB.size) return false
+
+    for (const value of objA) {
+      if (!objB.has(value)) {
+        return false
+      }
+    }
+    return true
+  }
+
   const keysA = Object.keys(objA)
   if (keysA.length !== Object.keys(objB).length) {
     return false
