307c025066d8e4cf8f32d2c4bb92b366f0ee969a
— <drcmda@gmail.com>
2023-07-16 14:54:03 +0200

    examples: fix prettier / lint

--- examples/demo/src/components/CopyButton.jsx
+++ examples/demo/src/components/CopyButton.jsx
@@ -34,7 +34,8 @@ export default function CopyButton({ code, ...props }) {
               strokeWidth={2}
               strokeLinecap="round"
               strokeLinejoin="round"
-              {...props}>
+              {...props}
+            >
               <rect x={9} y={9} width={13} height={13} rx={2} ry={2} />
               <path d="M5 15H4a2 2 0 01-2-2V4a2 2 0 012-2h9a2 2 0 012 2v1" />
             </svg>
