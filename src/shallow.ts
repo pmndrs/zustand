1f1a2aec045defa5cf85e403902b010e95c71231
FaberVitale <faber.vitale@gmail.com>
2023-09-30 16:06:02 +0200

    chore(useShallow): PR feedback https://github.com/pmndrs/zustand/pull/2090#discussion_r1341963105

--- src/shallow.ts
+++ src/shallow.ts
@@ -65,13 +65,10 @@ export default ((objA, objB) => {
 export function useShallow<S, U>(selector: (state: S) => U): (state: S) => U {
   const prev = useRef<U>()
 
-  return useMemo(
-    () => (state) => {
-      const next = selector(state)
-      return shallow(prev.current, next)
-        ? (prev.current as U)
-        : (prev.current = next)
-    },
-    [selector]
-  )
+  return (state) => {
+    const next = selector(state)
+    return shallow(prev.current, next)
+      ? (prev.current as U)
+      : (prev.current = next)
+  }
 }
