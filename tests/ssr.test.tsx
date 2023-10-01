2d89e10e66f6374176390f49e0fa6752dd8ca269
Jiwon Kim <lost731@naver.com>
2023-10-02 03:06:13 +0900

    test(ssr): Refactor SSR code using skipIf and importActual

--- tests/ssr.test.tsx
+++ tests/ssr.test.tsx
@@ -1,68 +1,75 @@
 import React, { useEffect } from 'react'
 import { act, screen } from '@testing-library/react'
 import { renderToString } from 'react-dom/server'
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 import { create } from 'zustand'
 
-const describeTest = React.version.startsWith('18') ? describe : describe.skip
+interface BearStoreState {
+  bears: number
+}
 
-describeTest('ssr behavior with react 18', () => {
-  interface BearStoreState {
-    bears: number
-  }
+interface BearStoreAction {
+  increasePopulation: () => void
+}
 
-  interface BearStoreAction {
-    increasePopulation: () => void
-  }
+const initialState = { bears: 0 }
+const useBearStore = create<BearStoreState & BearStoreAction>((set) => ({
+  ...initialState,
+  increasePopulation: () => set(({ bears }) => ({ bears: bears + 1 })),
+}))
 
-  const initialState = { bears: 0 }
-  const useBearStore = create<BearStoreState & BearStoreAction>((set) => ({
-    ...initialState,
-    increasePopulation: () => set(({ bears }) => ({ bears: bears + 1 })),
-  }))
+function Counter() {
+  const { bears, increasePopulation } = useBearStore(
+    ({ bears, increasePopulation }) => ({
+      bears,
+      increasePopulation,
+    })
+  )
 
-  function Counter() {
-    const { bears, increasePopulation } = useBearStore(
-      ({ bears, increasePopulation }) => ({
-        bears,
-        increasePopulation,
-      })
-    )
+  useEffect(() => {
+    increasePopulation()
+  }, [increasePopulation])
 
-    useEffect(() => {
-      increasePopulation()
-    }, [increasePopulation])
+  return <div>bears: {bears}</div>
+}
 
-    return <div>bears: {bears}</div>
-  }
+describe.skipIf(!React.version.startsWith('18'))(
+  'ssr behavior with react 18',
+  async () => {
+    it('should handle different states between server and client correctly', async () => {
+      const {
+        hydrateRoot,
+      }: {
+        hydrateRoot: (
+          container: Element | Document,
+          initialChildren: React.ReactNode
+        ) => void
+      } = await vi.importActual('react-dom/client')
 
-  it('should handle different states between server and client correctly', async () => {
-    const markup = renderToString(
-      <React.Suspense fallback={<div>Loading...</div>}>
-        <Counter />
-      </React.Suspense>
-    )
-
-    const container = document.createElement('div')
-    document.body.appendChild(container)
-    container.innerHTML = markup
-
-    expect(container.textContent).toContain('bears: 0')
-
-    await act(async () => {
-      // Using dynamic import for 'react-dom/client' as it’s not accessible in React versions below 18.
-      // eslint-disable-next-line import/no-unresolved
-      const { hydrateRoot } = await require('react-dom/client')
-      hydrateRoot(
-        container,
+      const markup = renderToString(
         <React.Suspense fallback={<div>Loading...</div>}>
           <Counter />
         </React.Suspense>
       )
-    })
 
-    const bearCountText = await screen.findByText('bears: 1')
-    expect(bearCountText).not.toBeNull()
-    document.body.removeChild(container)
-  })
-})
+      const container = document.createElement('div')
+      document.body.appendChild(container)
+      container.innerHTML = markup
+
+      expect(container.textContent).toContain('bears: 0')
+
+      await act(async () => {
+        hydrateRoot(
+          container,
+          <React.Suspense fallback={<div>Loading...</div>}>
+            <Counter />
+          </React.Suspense>
+        )
+      })
+
+      const bearCountText = await screen.findByText('bears: 1')
+      expect(bearCountText).not.toBeNull()
+      document.body.removeChild(container)
+    })
+  }
+)
