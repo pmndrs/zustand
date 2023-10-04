89215133180bdf1359565e295fe792997350154b
@dbritto-dev <dbritto.dev@gmail.com>
2023-10-04 10:50:58 -0500

    Minor changes

--- src/vanilla/shallow.ts
+++ src/vanilla/shallow.ts
@@ -47,15 +47,3 @@ export function shallow<T>(objA: T, objB: T) {
   }
   return true
 }
-
-/**
- * @deprecated Use `import { shallow } from 'zustand/shallow'`
- */
-export default ((objA, objB) => {
-  if (import.meta.env?.MODE !== 'production') {
-    console.warn(
-      "[DEPRECATED] Default export is deprecated. Instead use `import { shallow } from 'zustand/shallow'`."
-    )
-  }
-  return shallow(objA, objB)
-}) as typeof shallow
