c17d6c1ce87e09264f6af6a3b126c361719a9bf0
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2023-12-09 18:26:28 -0500

    refactor: export shallow in v5

--- src/shallow.ts
+++ src/shallow.ts
@@ -1,7 +1,2 @@
-import { shallow } from './vanilla/shallow.ts'
-
-// We will export this in v5 and remove default export
-// export { shallow } from './vanilla/shallow.ts'
-// export { useShallow } from './react/shallow.ts'
-
-export { shallow }
+export { shallow } from './vanilla/shallow.ts'
+export { useShallow } from './react/shallow.ts'
