b7a630d2c9b97f9ba1ef8ec4cfe73a7ad5b35957
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2023-12-12 08:28:53 -0600

    chore(test): switch type of test

--- tests/subscribe.test.tsx
+++ tests/subscribe.test.tsx
@@ -4,10 +4,6 @@ import { create } from 'zustand'
 describe('subscribe()', () => {
   it('should correctly have access to subscribe', () => {
     const { subscribe } = create(() => ({ value: 1 }))
-    expect({ subscribe }).toMatchInlineSnapshot(`
-    {
-      "subscribe": [Function],
-    }
-  `)
+    expect(typeof subscribe).toBe('function')
   })
 })
