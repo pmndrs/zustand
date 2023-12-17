34facb4f175ad6676bbd1056b06f28df3e2920b0
daishi <daishi@axlight.com>
2023-12-18 08:35:19 +0900

    drop ts <4.4

--- tests/types.test.tsx
+++ tests/types.test.tsx
@@ -112,9 +112,9 @@ it('should have correct (partial) types for setState', () => {
 
   const store = create<Count>((set) => ({
     count: 0,
-    // @ts-expect-error we shouldn't be able to set count to undefined [LATEST-TS-ONLY]
+    // @ts-expect-error we shouldn't be able to set count to undefined
     a: () => set(() => ({ count: undefined })),
-    // @ts-expect-error we shouldn't be able to set count to undefined [LATEST-TS-ONLY]
+    // @ts-expect-error we shouldn't be able to set count to undefined
     b: () => set({ count: undefined }),
     c: () => set({ count: 1 }),
   }))
@@ -130,9 +130,9 @@ it('should have correct (partial) types for setState', () => {
   store.setState({})
   store.setState((previous) => previous)
 
-  // @ts-expect-error type undefined is not assignable to type number [LATEST-TS-ONLY]
+  // @ts-expect-error type undefined is not assignable to type number
   store.setState({ count: undefined })
-  // @ts-expect-error type undefined is not assignable to type number [LATEST-TS-ONLY]
+  // @ts-expect-error type undefined is not assignable to type number
   store.setState((state) => ({ ...state, count: undefined }))
 })
 
