9c7b13e37beba7a6563d31d189ecfc94cfe162cd
Dominik Dorfmeister <office@dorfmeister.cc>
2023-12-30 12:07:34 +0100

    refactor: revert changes to oldImpl

--- src/middleware/persist.ts
+++ src/middleware/persist.ts
@@ -271,8 +271,6 @@ const oldImpl: PersistImpl = (config, baseOptions) => (set, get, api) => {
     api,
   )
 
-  api.getServerState = () => configResult
-
   // a workaround to solve the issue of not storing rehydrated state in sync storage
   // the set(state) value would be later overridden with initial state by create()
   // to avoid this, we merge the state from localStorage into the initial state.
