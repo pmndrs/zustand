b3b3cda346c0c46b26ac17d341e27d4075c2cc79
sunil <iamsunilsharma999@gmail.com>
2024-06-05 16:10:18 +0530

    update: example/demo
    - fix eslint config and lint issues
    - fix react key issue
    - remove children passed as props
    - remove unused variables and imports
    - "encodings_fragment" is deprecated, replaced it with "colorspace_fragment"

--- examples/demo/.eslintrc.cjs
+++ examples/demo/.eslintrc.cjs
@@ -1,20 +1,16 @@
 /* eslint-env node */
 
 module.exports = {
+  root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react/jsx-runtime', 'plugin:react-hooks/recommended'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react/prop-types': 0,
+    'import/extensions': ['off'],
+    'react/no-unknown-property': ['off'],
   },
 }
