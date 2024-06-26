a80061bee23d581d3aac73246ba1ef081c37d9a3
daishi <daishi@axlight.com>
2024-06-26 18:09:23 +0900

    fix merge main (prettier)

--- src/react.ts
+++ src/react.ts
@@ -22,7 +22,9 @@ type ReadonlyStoreApi<T> = Pick<
 >
 
 const identity = <T>(arg: T): T => arg
-export function useStore<S extends ReadonlyStoreApi<unknown>>(api: S): ExtractState<S>
+export function useStore<S extends ReadonlyStoreApi<unknown>>(
+  api: S,
+): ExtractState<S>
 
 export function useStore<S extends ReadonlyStoreApi<unknown>, U>(
   api: S,
