efa1f02c8934f5b95ace75e3f41a7fe02b11456e
Daishi Kato <dai-shi@users.noreply.github.com>
2024-06-29 09:21:02 +0900

    Update tests/middlewareTypes.test.tsx

--- tests/middlewareTypes.test.tsx
+++ tests/middlewareTypes.test.tsx
@@ -112,7 +112,7 @@ describe('counter state spec (single middleware)', () => {
     }
     TestComponent
 
-    const _testSubtyping: StoreApi<{ count: number }> = createStore(
+    const _testSubtyping: StoreApi<object> = createStore(
       devtools(() => ({ count: 0 })),
     )
   })
