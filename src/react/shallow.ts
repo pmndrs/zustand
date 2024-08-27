f83d4acc6d952d84f01db31c4118c6d848a0ad2c
daishi <daishi@axlight.com>
2024-08-27 10:07:47 +0900

    refactor useShallow

--- src/react/shallow.ts
+++ src/react/shallow.ts
@@ -6,15 +6,17 @@ import { shallow } from '../vanilla/shallow.ts'
 
 const { useRef } = ReactExports
 
-export function useShallow<S, U>(selector: (state: S) => U): (state: S) => U {
-  const prev = useRef<U>()
+const sliceCache = new WeakMap<object, unknown>()
 
+export function useShallow<S, U>(selector: (state: S) => U): (state: S) => U {
+  const key = useRef({}).current
   return (state) => {
+    const prev = sliceCache.get(key) as U | undefined
     const next = selector(state)
-    return shallow(prev.current, next)
-      ? (prev.current as U)
-      : // It might not work with React Compiler
-        // eslint-disable-next-line react-compiler/react-compiler
-        (prev.current = next)
+    if (shallow(prev, next)) {
+      return prev as U
+    }
+    sliceCache.set(key, next)
+    return next
   }
 }
