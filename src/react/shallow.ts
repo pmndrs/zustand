c08adc2a474b2ce2714957fc1df43e4eef52be2d
Daishi Kato <dai-shi@users.noreply.github.com>
2024-09-15 11:19:05 +0900

    revert useShallow refactor in #2701 (#2703)

--- src/react/shallow.ts
+++ src/react/shallow.ts
@@ -1,22 +1,12 @@
-// import { useRef } from 'react'
-// That doesnt work in ESM, because React libs are CJS only.
-// The following is a workaround until ESM is supported.
-import ReactExports from 'react'
+import React from 'react'
 import { shallow } from '../vanilla/shallow.ts'
 
-const { useRef } = ReactExports
-
-const sliceCache = new WeakMap<object, unknown>()
-
 export function useShallow<S, U>(selector: (state: S) => U): (state: S) => U {
-  const key = useRef({}).current
+  const prev = React.useRef<U>()
   return (state) => {
-    const prev = sliceCache.get(key) as U | undefined
     const next = selector(state)
-    if (shallow(prev, next)) {
-      return prev as U
-    }
-    sliceCache.set(key, next)
-    return next
+    return shallow(prev.current, next)
+      ? (prev.current as U)
+      : (prev.current = next)
   }
 }
