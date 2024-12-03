f4c62d43081ff5d47910bb678f90b7d6245d3184
Danilo Britto <dbritto.dev@gmail.com>
2024-12-02 20:28:49 -0500

    feat: minor changes

--- tests/persistAsync.test.tsx
+++ tests/persistAsync.test.tsx
@@ -336,9 +336,11 @@ describe('persist middleware with async configuration', () => {
     )
 
     await screen.findByText('count: 0')
+
     await waitFor(() => {
       expect(console.error).toHaveBeenCalled()
     })
+
     await waitFor(() => {
       expect(onRehydrateStorageSpy).toBeCalledWith({ count: 0 }, undefined)
     })
