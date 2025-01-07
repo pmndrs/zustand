6eff6e7eec8755bc5d9366070ec92131fdbff967
Daishi Kato <dai-shi@users.noreply.github.com>
2025-01-07 23:17:07 +0900

    chore(deps): update dev dependencies (#2944)

--- src/react/shallow.ts
+++ src/react/shallow.ts
@@ -2,7 +2,7 @@ import React from 'react'
 import { shallow } from '../vanilla/shallow.ts'
 
 export function useShallow<S, U>(selector: (state: S) => U): (state: S) => U {
-  const prev = React.useRef<U>()
+  const prev = React.useRef<U>(undefined)
   return (state) => {
     const next = selector(state)
     return shallow(prev.current, next)
