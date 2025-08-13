a50ad05532cde172843c3ccf75c8eaf4805c0afc
Triumph-light <2495685883@qq.com>
2025-08-13 23:09:16 +0800

    feat: add zustand playground

--- examples/demo/src/components/Details.jsx
+++ examples/demo/src/components/Details.jsx
@@ -3,6 +3,7 @@ export default function Details() {
     <>
       <nav className="nav">
         <a href="https://zustand.docs.pmnd.rs/">Documentation</a>
+        <a href="https://react-repl.vercel.app/#eNp9VE1v2zAM/SuEL3Ww1G4GDBvSJOtQ9NAB64Z2t3kHV2YStzZl6CPLZvi/j5IVN0mDHhJIfHzk46PgNvrSNInR22jaRsuyQsprjKZDdBwJWbhAWTdSGbjHXBhYKlnDmXLns8uMAtaC4JBB6ELCP6tNTsVZRhmZvw3Cg5EKYQ5tRgBCWjJTIFs/onKBksQU4hHMF7CRZZFR54hCkjZgNe7IfZOZvy7iURxrNJ4U75edjI9KchanGqaG5F2mjyX+Au9gAt1oxFz+d92XloQpJcG1w1FxrdDFqQo1xq4PTz0fZMZMBlBorCKI3RlgVpSbRX/ki25yWrSe3s1SfxuwR2sMt5R0XZXied5y9W4hCcE2s7QHQ/Is3RXlhmxXL4uXx1r6sb3aIyFhlrQn8gI7/mWEW7/GApe5rXyVy6gbh82f13mTPGlJRw/lGB0ejG+cBVxn0bSX4mL+4bhIFq2NafQ0TVHXiV6nHrmafEreJxdZ5Fc4EM4LWaeiKpHe4Lqk1/zwEE/SAnb1IblIPn7GLRtDeTUPGl0F9rVzRhjN9i7L1SkbDrFjE4SsG85V3xv3lg7NyKtK/vnqY0ZZHDSLNYrnE/Enve3n+KFQo9rg3pwmVysM7tw83PEwe2AtC1tx9hvgPWpZWaexT7vjKZyEIc2LvfUrLWn1U9+wXaR3Mx3oRP3N17x1T002r3D3QfCsXy8rEQWxgQVW5UYlhCalpj5cUFpSgduk4I9TFv1+2U73H7+AkXM=">Playground</a>
         <a href="https://github.com/pmndrs/zustand">Github</a>
       </nav>
       <div className="bottom">
