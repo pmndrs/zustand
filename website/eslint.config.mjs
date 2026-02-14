b9d75be1418776e6a6fddd4849de7177653e2746
Danilo Britto <dbritto.dev@gmail.com>
2026-02-13 21:17:33 -0500

    feat: add rspress website

---
+++ website/eslint.config.mjs
@@ -0,0 +1,10 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import ts from 'typescript-eslint';
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  { ignores: ['dist/'] },
+];
